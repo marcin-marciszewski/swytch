55456919e22865fb3f81b36c52cf7408b1469f8f
szantilas87 <szantilas@gmail.com>
2020-07-03 09:43:03 +0100

    Description

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Change subpages
 $(document).ready(function () {
-
+    // Play video start
     $('#video, #overlay').fadeIn('slow');
     $('#video-container').html('<iframe width=300 height=315 src=https://www.youtube.com/embed/KEgxcv-NAzY frameborder=0 allowfullscreen></iframe>');
+
+    // Open subpage
     var trigger = $('#content .tabs a'),
         tabs_content = $('#tabs-content')
-    tabs_content.load('subpages/video.html');
+    tabs_content.load('subpages/description.html');
 
     trigger.on('click', function () {
         var $this = $(this),
@@ -16,7 +18,7 @@ $(document).ready(function () {
     })
 });
 
-
+// Close video
 $(document).on('touchend, mouseup', function (e) {
     if (!$('#video').is(e.target)) {
         $('#video, #overlay').fadeOut('slow');
@@ -32,4 +34,38 @@ function foo() {
     setTimeout(function () {
         console.log("aaa");
     }, 3000);
+}
+
+
+// Gallery
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
