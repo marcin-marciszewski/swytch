618796076a24bc38ef75cc257270434576af4acb
szantilas87 <szantilas@gmail.com>
2020-07-06 16:52:46 +0100

    Media queries

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 // Change subpages
 $(document).ready(function () {
+    if ($(window).width() < 376 && $(window).height() < 668) {
+        console.log("object")
+        document.querySelector('#checkout').style.marginTop = "550px";
+    }
     // Play video start
     $('#video, #overlay').fadeIn('slow');
     $('#video-container').html('<iframe width=300 height=315 src=https://www.youtube.com/embed/KEgxcv-NAzY frameborder=0 allowfullscreen></iframe>');
@@ -12,13 +16,42 @@ $(document).ready(function () {
     trigger.on('click', function () {
         var $this = $(this),
             target = $this.data('target');
-        console.log('subpages/' + target + '.html')
+
         tabs_content.load('subpages/' + target + '.html');
+
+        if ($(window).width() < 376 && $(window).height() < 813 && target === "gallery") {
+            document.querySelector('#checkout').style.marginTop = "-150px";
+        }
+
+        if ($(window).width() < 376 && $(window).height() < 813 && target === "mission") {
+            document.querySelector('#checkout').style.marginTop = "350px";
+        }
+
+        if ($(window).width() < 376 && $(window).height() < 813 && target === "description") {
+            document.querySelector('#checkout').style.marginTop = "450px";
+        }
+
+        if ($(window).width() < 376 && $(window).height() < 668 && target === "gallery") {
+            document.querySelector('#checkout').style.marginTop = "-100px";
+        }
+
+        if ($(window).width() < 376 && $(window).height() < 668 && target === "mission") {
+            document.querySelector('#checkout').style.marginTop = "350px";
+            document.querySelector('.tabs').style.marginBottom = "40px";
+        }
+
+        if ($(window).width() < 376 && $(window).height() < 668 && target === "description") {
+            document.querySelector('#checkout').style.marginTop = "550px";
+        }
+
         return false;
+
+
     })
 });
 
 $(document).on('touchend, mouseup', function (e) {
+    document.querySelector('#start-container').style.display = 'none';
     const mainContent = document.querySelector('.main_content');
     const pluses = document.querySelectorAll('.fa-plus');
 
@@ -40,12 +73,10 @@ $(document).on('touchend, mouseup', function (e) {
 
         seconds += 500;
     }
-
     // Count down the price
     setTimeout(function () {
         updatePrice();
     }, 1000);
-
 
 });
 
@@ -96,3 +127,12 @@ btnPay.addEventListener('click', (e) => {
     })
     e.preventDefault();
 });
+
+// 
+// if ($(window).width() < 500 ) {
+//     document.querySelector('#checkout').style.marignTop = "400px";
+//  }
+
+if ($("#description").is(":visible") == true) {
+    alert("Div is visible!!");
+}
